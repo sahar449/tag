fa105811a209e139ae92172033d2a3541787038c
sahar449 <saharr449@gmail.com>
2024-03-06 13:35:24 +0200

    .

--- main.tf
+++ main.tf
@@ -7,4 +7,11 @@ provider "aws" {
 resource "aws_instance" "test" {
   ami = "ami-0440d3b780d96b29d"
   instance_type = "t2.micro"
+  tags = {
+    Name = var.name
+  }
+}
+
+variable "name" {
+  
 }
